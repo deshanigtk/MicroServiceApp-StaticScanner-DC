0f77b5341928d9c8d524ea524e112a9a78e2dd4c
deshanigtk <deshani.14@cse.mrt.ac.lk>
2017-09-10 19:49:57 +0530

    Add licence header

--- src/main/java/org/wso2/security/handlers/MavenHandler.java
+++ src/main/java/org/wso2/security/handlers/MavenHandler.java
@@ -5,14 +5,27 @@ import java.io.File;
 import java.util.Collections;
 import java.util.OptionalInt;
 
-/**
- * Created by deshani on 8/2/17.
- */
+/*
+*  Copyright (c) ${date}, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+*  WSO2 Inc. licenses this file to you under the Apache License,
+*  Version 2.0 (the "License"); you may not use this file except
+*  in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*    http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied.  See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
 
 public class MavenHandler {
 
     private static final String MVN_COMMAND_M2_HOME = "M2_HOME";
-
 
     public static void runMavenCommand(String pomFilePath, String mavenCommand) throws MavenInvocationException {
         InvocationRequest request = new DefaultInvocationRequest();
@@ -24,36 +37,4 @@ public class MavenHandler {
         InvocationResult result = invoker.execute(request);
         OptionalInt.of(result.getExitCode());
     }
-
-//    public static void buildDependencyCheck(String pomFilePath) throws MavenInvocationException {
-//        InvocationRequest request = new DefaultInvocationRequest();
-//        request.setPomFile(new File(pomFilePath));
-//        request.setGoals(Collections.singletonList(MVN_COMMAND_DEPENDENCY_CHECK));
-//
-//        Invoker invoker = new DefaultInvoker();
-//        invoker.setMavenHome(new File(System.getenv(MVN_COMMAND_M2_HOME)));
-//        InvocationResult result = invoker.execute(request);
-//        OptionalInt.of(result.getExitCode());
-//    }
-//
-//    public static void compile(String pomFilePath) throws MavenInvocationException {
-//        InvocationRequest request = new DefaultInvocationRequest();
-//        request.setPomFile(new File(pomFilePath));
-//        request.setGoals(Collections.singletonList(MVN_COMMAND_COMPILE));
-//
-//        Invoker invoker = new DefaultInvoker();
-//        invoker.setMavenHome(new File(System.getenv(MVN_COMMAND_M2_HOME)));
-//        invoker.execute(request);
-//    }
-//
-//    public static void buildFindSecBugs(String pomFilePath) throws MavenInvocationException {
-//        InvocationRequest request = new DefaultInvocationRequest();
-//        request.setPomFile(new File(pomFilePath));
-//        request.setGoals(Collections.singletonList(MVN_COMMAND_FIND_SEC_BUGS));
-//
-//        Invoker invoker = new DefaultInvoker();
-//
-//        invoker.setMavenHome(new File(System.getenv(MVN_COMMAND_M2_HOME)));
-//        invoker.execute(request);
-//    }
 }
